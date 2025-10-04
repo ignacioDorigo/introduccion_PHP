47b0f9ea98b779e44aa77bc4d8f133eea74da4b0
ignacioDorigo <71302004+ignacioDorigo@users.noreply.github.com>
2025-10-03 22:29:03 -0300

    Tipos de datos

--- 03-tipos-dato.php
+++ 03-tipos-dato.php
@@ -1,6 +1,36 @@
 <?php include 'includes/header.php';
 
 
+$nombre = "Ignacio";    // string
+var_dump($nombre);
+echo "<br>";
 
+$edad = 25;             // int
+var_dump($edad);
+echo "<br>";
+
+$altura = 1.75;         // float
+var_dump($altura);
+echo "<br>";
+
+$activo = true;         // boolean
+var_dump($activo);
+echo "<br>";
+
+$colores = ["rojo","azul","verde"]; // array
+var_dump($colores);
+echo "<br>";
+
+$persona = (object) ["nombre"=>"Ignacio","edad"=>25]; // object
+var_dump($persona);
+echo "<br>";
+
+$vacante = null;        // null
+var_dump($vacante);
+echo "<br>";
 
 include 'includes/footer.php';
+
+?>
+
+
