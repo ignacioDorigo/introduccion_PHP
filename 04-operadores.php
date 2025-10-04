92928c818dc2b022cd3fde4bb4d4f949d1bbd422
ignacioDorigo <71302004+ignacioDorigo@users.noreply.github.com>
2025-10-03 22:49:48 -0300

    Algunos operadores

--- 04-operadores.php
+++ 04-operadores.php
@@ -1,5 +1,38 @@
 <?php include 'includes/header.php';
 
+$numero1 = 3;
+$numero2 = 4;
+$numero3 = 5;
+$numero4 = 4;
+
+var_dump($numero1 + $numero2);
+echo "<br>";
+var_dump($numero1 - $numero2);
+echo "<br>";
+var_dump($numero1 / $numero2);
+echo "<br>";
+var_dump($numero2 % $numero4);
+echo "<br>";
+var_dump($numero2 * $numero4);
+echo "<br>";
+
+var_dump($numero2 ** $numero4);
+echo "<br>";
+
+
+echo "$numero2 <=> $numero4 ==> ";
+var_dump($numero2 <=> $numero4);
+echo "<br>";
+
+echo "$numero1 <=> $numero3 ==> ";
+var_dump($numero1 <=> $numero3);
+echo "<br>";
+
+echo "$numero3 <=> $numero1 ==> ";
+var_dump($numero3 <=> $numero1);
+echo "<br>";
+
+
 
 
 
