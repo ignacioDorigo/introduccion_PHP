4d2b0f29e6e7cfd53c7943b90df25f772ef4b14b
ignacioDorigo <71302004+ignacioDorigo@users.noreply.github.com>
2025-10-03 22:25:21 -0300

    VARIABLES EN PHP

--- 02-variables.php
+++ 02-variables.php
@@ -1,6 +1,16 @@
 <?php include 'includes/header.php';
 
 
+$nombre = "Ignacio";
+$apellido = "Dorigo";
+$edad = 25;
 
+include 'includes/footer.php'; ?>
 
-include 'includes/footer.php';
+<div>
+    <h1>Hola <?php
+                echo $nombre . " " . $apellido ?></h1>
+    <h2>Asi que tenes
+        <?php
+        echo $edad . " años" ?></h2>
+</div>
