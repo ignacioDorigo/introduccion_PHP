4fc1e9715b96b1f33939b4682cffded334379457
ignacioDorigo <71302004+ignacioDorigo@users.noreply.github.com>
2025-10-03 22:53:44 -0300

    incremento y decremento

--- 06-inc-dec.php
+++ 06-inc-dec.php
@@ -1,6 +1,11 @@
 <?php include 'includes/header.php';
 
 
+$a = 5;
+echo ++$a; // 6 (primero suma, luego imprime)
+echo $a++; // 6 (primero imprime, luego suma)
+echo $a;   // 7 (valor final después del post-incremento)
 
+// Hacel omismo para la resta
 
 include 'includes/footer.php';
